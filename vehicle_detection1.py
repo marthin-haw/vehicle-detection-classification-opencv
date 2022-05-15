783702432b83059d8f92321eecb93592ed84cccb
marthin-haw <marthinhawila32000@gmail.com>
2022-05-15 22:12:08 +0700

    Program menghitung jalur Utara. Sudah menggunakan cuda. menggunakan Yolo-320

--- vehicle_detection1.py
+++ vehicle_detection1.py
@@ -8,44 +8,43 @@ from tracker import *
 tracker = EuclideanDistTracker()
 
 # Model, weight, and classfile
-modelconfig = "yolov4-tiny.cfg"
-modelweight = "yolov4-tiny.weights"
+modelconfig = "yolov4-320.cfg"
+modelweight = "yolov4.weights"
 classfile = "coco.names"
 classnames = open(classfile).read().strip().split("\n")
 net = cv2.dnn.readNetFromDarknet(modelconfig, modelweight)
 
+# Set backend and target to CUDA to use GPU
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+
 # Class index for our required detection classes
 required_class_index = [2, 3, 5, 7]
+
+# List every detected class
 detected_classnames = []
 
 # Load video
-cap = cv2.VideoCapture("vid1.mp4")
+cap = cv2.VideoCapture("C:\\Users\marth\Documents\Tugas Akhir\CCTV\ch84_20220427070000.mp4")
 input_size = 320
 
 # Detection confidence threshold
 confthreshold = 0.2
 nmsthreshold = 0.2
 
-left_x1 = 415 ; left_x2 = 510
-straight_x1 = 315 ; straight_x2 = 415
-right_x1 = 220  ; right_x2 = 315
-line = 255
+# Route
+intersection = "North"
+left = "route_11"
+straight = "route_9"
+right = "route_10"
+
+# Line to count vehicle
+start_border = 460
+end_border = 1250
+d_1 = 680
+d_2 = 945
+line = 250
 error = 15
-
-font = cv2.FONT_HERSHEY_SIMPLEX
-font_size = 0.5
-font_thickness = 2
-font_color = (0, 0, 255)
-colors = np.random.randint(0, 255, size=(len(classnames), 3), dtype='uint8')
-
-total_time = []
-
-def find_center(x, y, w, h):
-    x1 = int(w/2)
-    y1 = int(h/2)
-    cx = x + x1
-    cy = y + y1
-    return cx, cy
 
 # Update list vehicle
 detected_id = []
@@ -54,25 +53,41 @@ straight_list = [0, 0, 0, 0]
 left_list = [0, 0, 0, 0]
 direction_list = []
 
+# Time every vehicle detected
+total_time = []
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+font_size = 0.5
+font_thickness = 2
+font_color = (0, 0, 255)
+colors = np.random.randint(0, 255, size=(len(classnames), 3), dtype='uint8')
+
+def find_center(x, y, w, h):
+    x1 = int(w/2)
+    y1 = int(h/2)
+    cx = x + x1
+    cy = y + y1
+    return cx, cy
+
 def count_vehicle(box_id, img):
     global detect_time
     x, y, w, h, id, index = box_id
     center = find_center(x, y, w, h)
     ix, iy = center
-    name_class = ["car", "motorbike", "bus", "truck"]
+    name_class = ["car", "motorcycle", "bus", "truck"]
 
-    if (iy > (line - error)) and (iy < line):
+    if ((iy > (line - error)) and iy < line) and ((ix > start_border) and (ix < end_border)):
         if id not in detected_id:
             detected_id.append(id)
-            if (ix > left_x1) and (ix < left_x2):
+            if (ix >= d_2) and (ix < end_border):
                 left_list[index] = left_list[index]+1
-                direction_list.append("left")
-            if (ix > straight_x1) and (ix < straight_x2):
+                direction_list.append(left)
+            if (ix >= d_1) and (ix < d_2):
                 straight_list[index] = straight_list[index]+1
-                direction_list.append("straight")
-            if (ix > right_x1) and (ix < right_x2):
+                direction_list.append(straight)
+            if (ix > start_border) and (ix < d_1):
                 right_list[index] = right_list[index]+1
-                direction_list.append("right")
+                direction_list.append(right)
             detect_time = timeit.default_timer() - start_time
             total_time.append(detect_time)
             detected_classnames.append(name_class[index])
@@ -112,11 +127,11 @@ def postprocess(outputs, img):
         count_vehicle(box_id, img)
 
 def realTime():
+    global start_time
+    start_time = timeit.default_timer()
     while True:
-        global start_time 
-        start_time = timeit.default_timer()
         success, img = cap.read()
-        img = cv2.resize(img, (0,0), None, 0.5, 0.5)
+        img = cv2.resize(img, (0,0), None, 0.35, 0.35)
         ih, iw, channels = img.shape
         blob = cv2.dnn.blobFromImage(img, 1/255, (input_size, input_size), [0, 0, 0], 1, crop=False)
         net.setInput(blob)
@@ -125,23 +140,23 @@ def realTime():
         outputs = net.forward(outputnames)
         postprocess(outputs, img)
 
-        cv2.line(img, (left_x1, line), (left_x2, line), (255, 0, 0), 3)
-        cv2.line(img, (straight_x1, line), (straight_x2, line), (0, 255, 0), 3)
-        cv2.line(img, (right_x1, line), (right_x2, line), (0, 0, 255), 3)
+        cv2.line(img, (d_2, line), (end_border, line), (255, 0, 0), 3)
+        cv2.line(img, (d_1, line), (d_2, line), (0, 255, 0), 3)
+        cv2.line(img, (start_border, line), (d_1, line), (0, 0, 255), 3)
 
         cv2.putText(img, "Right", (110, 20), font, font_size, font_color, font_thickness)
         cv2.putText(img, "Straight", (160, 20), font, font_size, font_color, font_thickness)
         cv2.putText(img, "Left", (230, 20), font, font_size, font_color, font_thickness)
         cv2.putText(img, "Car:         "+str(right_list[0])+"      "+str(straight_list[0])+"      "+str(left_list[0]), (20, 40), font, font_size, font_color, font_thickness)
-        cv2.putText(img, "Motorbike:   "+str(right_list[1])+"      "+str(straight_list[1])+"      "+str(left_list[1]), (20, 60), font, font_size, font_color, font_thickness)
+        cv2.putText(img, "Motorcycle:  "+str(right_list[1])+"      "+str(straight_list[1])+"      "+str(left_list[1]), (20, 60), font, font_size, font_color, font_thickness)
         cv2.putText(img, "Bus:         "+str(right_list[2])+"      "+str(straight_list[2])+"      "+str(left_list[2]), (20, 80), font, font_size, font_color, font_thickness)
         cv2.putText(img, "Truck:       "+str(right_list[3])+"      "+str(straight_list[3])+"      "+str(left_list[3]), (20, 100), font, font_size, font_color, font_thickness)
 
         cv2.imshow('Output', img)
-        print(detected_id)
+        '''print(detected_id)
         print(total_time)
         print(detected_classnames)
-        print(direction_list)
+        print(direction_list)'''
         if cv2.waitKey(1) == ord('q'):
             break
 
@@ -152,7 +167,7 @@ def realTime():
         direction_list.insert(0, "Direction")
         total_time.insert(0, "Time")
         for i in range(len(detected_id)):
-            cwriter.writerow([detected_id[i], detected_classnames[i], direction_list[i], total_time[i]])
+            cwriter.writerow(["North_"+str(detected_id[i]), detected_classnames[i], direction_list[i], total_time[i]])
     f1.close()
     print("Data saved at 'data1.csv'")
 
